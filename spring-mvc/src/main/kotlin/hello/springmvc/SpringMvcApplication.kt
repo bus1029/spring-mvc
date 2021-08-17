428743979d9f0ec34d85dbfe932cf9ca81d33165
Seokhyun Bae <bus1029@gmail.com>
2021-08-17 15:37:09 +0900

    feat: Welcome Page 생성 및 프로젝트 준비
    - Jar 프로젝트는 static/index.html을 시작 페이지로 설정

---
+++ spring-mvc/src/main/kotlin/hello/springmvc/SpringMvcApplication.kt
@@ -0,0 +1,11 @@
+package hello.springmvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvcApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringMvcApplication>(*args)
+}
