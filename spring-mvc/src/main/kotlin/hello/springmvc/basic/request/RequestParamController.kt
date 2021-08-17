56a55a80ef79c176f6698e84d29fe663474b5da6
Seokhyun Bae <bus1029@gmail.com>
2021-08-17 18:38:53 +0900

    feat: RequestParam의 여러 옵션들 학습
    - RequestParam에서 사용할 수 있는 여러 옵션들과 사용 방법들 테스트

--- spring-mvc/src/main/kotlin/hello/springmvc/basic/request/RequestParamController.kt
+++ spring-mvc/src/main/kotlin/hello/springmvc/basic/request/RequestParamController.kt
@@ -3,6 +3,8 @@ package hello.springmvc.basic.request
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -17,5 +19,51 @@ class RequestParamController {
     logger.info("username={}, age={}", username, age)
 
     response?.writer?.write("OK")
+  }
+
+  @ResponseBody // 해당 Annotation 으로 View 를 찾지 않고 Http Body 에 문자열 담아서 반환
+  @RequestMapping("/request-param-v2")
+  fun requestParamV2(@RequestParam("username") memberName: String,
+                     @RequestParam("age") memberAge: Int): String {
+    logger.info("username={}, age={}", memberName, memberAge)
+    return "ok"
+  }
+
+  @ResponseBody // 해당 Annotation 으로 View 를 찾지 않고 Http Body 에 문자열 담아서 반환
+  @RequestMapping("/request-param-v3")
+  fun requestParamV3(@RequestParam username: String,
+                     @RequestParam age: Int): String {
+    logger.info("username={}, age={}", username, age)
+    return "ok"
+  }
+
+  @ResponseBody // 해당 Annotation 으로 View 를 찾지 않고 Http Body 에 문자열 담아서 반환
+  @RequestMapping("/request-param-v4")
+  fun requestParamV4(username: String, age: Int): String {
+    logger.info("username={}, age={}", username, age)
+    return "ok"
+  }
+
+  @ResponseBody // 해당 Annotation 으로 View 를 찾지 않고 Http Body 에 문자열 담아서 반환
+  @RequestMapping("/request-param-required")
+  fun requestParamRequired(@RequestParam(required = true) username: String,
+                           @RequestParam(required = false) age: Int?): String {
+    logger.info("username={}, age={}", username, age)
+    return "ok"
+  }
+
+  @ResponseBody // 해당 Annotation 으로 View 를 찾지 않고 Http Body 에 문자열 담아서 반환
+  @RequestMapping("/request-param-default")
+  fun requestParamDefault(@RequestParam(defaultValue = "guest") username: String,
+                           @RequestParam(defaultValue = "-1") age: Int): String {
+    logger.info("username={}, age={}", username, age)
+    return "ok"
+  }
+
+  @ResponseBody // 해당 Annotation 으로 View 를 찾지 않고 Http Body 에 문자열 담아서 반환
+  @RequestMapping("/request-param-map")
+  fun requestParamMap(@RequestParam paramMap: Map<String, Any?>): String {
+    logger.info("username={}, age={}", paramMap["username"], paramMap["age"])
+    return "ok"
   }
 }
